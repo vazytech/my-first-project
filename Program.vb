2055def1b20a05f7271a60092b0917f4b1695901
vazy_tech <martingatimu69@gmail.com>
2025-02-27 20:18:54 +0300

    my first change

---
+++ Program.vb
@@ -0,0 +1,27 @@
+﻿'
+' Created by SharpDevelop.
+' User: 27465
+' Date: 1/29/2025
+' Time: 2:18 PM
+' 
+' To change this template use Tools | Options | Coding | Edit Standard Headers.
+'
+Imports Microsoft.VisualBasic.ApplicationServices
+
+Namespace My
+	' This file controls the behaviour of the application.
+	Partial Class MyApplication
+		Public Sub New()
+			MyBase.New(AuthenticationMode.Windows)
+			Me.IsSingleInstance = False
+			Me.EnableVisualStyles = True
+			Me.SaveMySettingsOnExit = True
+			Me.ShutDownStyle = ShutdownMode.AfterMainFormCloses
+		End Sub
+		
+		
+		Protected Overrides Sub OnCreateMainForm()
+			Me.MainForm = My.Forms.MainForm
+		End Sub
+	End Class
+End Namespace
